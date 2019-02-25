7461a0ff560e14242d02b5cc683412fd69ce0da9
Gvidas <gvidaxs@gmail.com>
2019-02-25 18:53:33 +0200

    added router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-
+import { BrowserRouter, Route } from 'react-router-dom';
 import LoginButton from './Navbar/Login';
 
 import Store from './redux/store';
 
+const a = () => <div />;
 export default () => (
   <Provider store={Store}>
     <LoginButton />
+    <BrowserRouter>
+      <Route path="/" component={a} />
+    </BrowserRouter>
   </Provider>
 );
