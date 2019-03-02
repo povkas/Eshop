d4da6e246a44bed6f280fca7f7319d3179acb72c
DeiVarnagis <47643602+DeiVarnagis@users.noreply.github.com>
2019-03-02 17:32:35 +0200

    Add files via upload

---
+++ Back-end/Eshop/Models/OurDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace eShop_RegLog.Models
+{
+    public class OurDbContext : DbContext
+    {
+        public DbSet<UserAccount> userAcount { get; set; }
+
+    }
+}
---
+++ Back-end/Eshop/Models/UserAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace eShop_RegLog.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserID { get; set; }
+        [Required(ErrorMessage = " Name is required ")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = " Last name is required ")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = " Email address is required ")]
+        [EmailAddress(ErrorMessage = " PLease enter valid email address ")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = " Country is required ")]
+        public string Country { get; set; }
+        [Required(ErrorMessage = " City is required ")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = " Password is required ")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Compare("Password", ErrorMessage = " Please confirm your password ")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
