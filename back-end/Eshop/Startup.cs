ac40f1bb44c50119253bb3b30be6da1ff24dee1c
Povilas <p.krasavinas@gmail.com>
2019-03-02 23:36:09 +0200

    Restoring changes

--- back-end/Eshop/Startup.cs
+++ back-end/Eshop/Startup.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Eshop.Configurations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,7 +25,6 @@ namespace Eshop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.SetUpDatabase(Configuration);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -44,7 +42,6 @@ namespace Eshop
 
             app.UseHttpsRedirection();
             app.UseMvc();
-            app.InitializeDatabase();
         }
     }
 }
