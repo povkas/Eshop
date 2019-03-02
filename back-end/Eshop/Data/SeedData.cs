8c699f2c4864fe28c1fc2361fec57c5c86f674c7
Povilas <p.krasavinas@gmail.com>
2019-03-02 15:35:05 +0200

    Added database models and  a seed

---
+++ back-end/Eshop/Data/SeedData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Eshop.Models;
+
+namespace Eshop.Data
+{
+    public class SeedData
+    {
+        public static void Seed(Context context)
+        {
+            if (context.Users.Any())
+                return;
+
+            var users = new List<User>
+            {
+                new User{Email = "admin@gmail.com", Password= "admin", Role = "ADMIN"},
+                new User{Email = "user@gmail.com", Password= "user", Role = "USER"}
+            };
+
+            users.ForEach(t => context.Users.Add(t));
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ back-end/Eshop/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eshop.Models
+{
+    public class Account
+    {
+        public int User_id { get; set; }
+        public string Address { get; set; }
+    }
+}
