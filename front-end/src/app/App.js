e336bd780cf7febc1b0f8276c4f8c6783db37776
Gvidas <gvidaxs@gmail.com>
2019-03-05 16:10:33 +0200

    added NavBarContainer

--- front-end/src/app/App.js
+++ front-end/src/app/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
-import LoginButton from '../components/Navbar/Login';
+import NavBar from '../components/Navbar/NavBarContainer';
 
 import Store from '../utils/redux/store';
 
 const a = () => <div />;
 export default () => (
   <Provider store={Store}>
-    <LoginButton />
+    <NavBar />
     <BrowserRouter>
       <Route path="/" component={a} />
     </BrowserRouter>
---
+++ front-end/src/components/Navbar/NavBarContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+function NavBarContainer() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+export default NavBarContainer;
