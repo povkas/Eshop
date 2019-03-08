d89888a0f691617ce60b1e398732bccf0a3c8be0
Povilas <p.krasavinas@gmail.com>
2019-03-08 22:39:53 +0200

    Added ProductDto

---
+++ back-end/Eshop/DTOs/ProductDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Eshop.Temp;
+
+namespace Eshop.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public ProductCategory Category { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
