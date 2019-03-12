41c69e284ead75c7b4cfbc9a9e4ace3baa985a5a
Gvidas <gvidaxs@gmail.com>
2019-03-12 19:32:44 +0200

    removing alias again

--- front-end/src/app/App.js
+++ front-end/src/app/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { NavBarContainer } from 'Components/Navbar';
+import { NavBarContainer } from '../components/Navbar';
 import Store from '../utils/redux/store';
 
 const a = () => <div />;
