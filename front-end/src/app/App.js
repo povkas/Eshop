90daf655d7729aa55f97f028d23e91e38c388a02
Gvidas <gvidaxs@gmail.com>
2019-03-12 19:05:42 +0200

    removed alias, to check if travis works

--- front-end/src/app/App.js
+++ front-end/src/app/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { NavBarContainer } from 'Components/navbar';
+import { NavBarContainer } from '../components/navbar';
 import Store from '../utils/redux/store';
 
 const a = () => <div />;
