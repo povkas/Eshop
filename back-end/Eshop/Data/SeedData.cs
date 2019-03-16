01826a3e4a144b5e14c16cea79ddaf86fdbd06f4
Povilas <p.krasavinas@gmail.com>
2019-03-16 15:43:26 +0200

    Added seed for product categories and a postman request to the api

--- back-end/Eshop/Data/SeedData.cs
+++ back-end/Eshop/Data/SeedData.cs
@@ -9,7 +9,7 @@ namespace Eshop.Data
     {
         public static void Seed(Context context)
         {
-            if (context.Users.Any() && context.Products.Any())
+            if (context.Users.Any() && context.Products.Any() && context.ProductCategories.Any())
                 return;
 
             var users = new List<User>
@@ -26,6 +26,19 @@ namespace Eshop.Data
             };
 
             products.ForEach(t => context.Products.Add(t));
+
+            var productCategories = new List<ProductCategory>
+            {
+                new ProductCategory(){Category = "Electronics"},
+                new ProductCategory(){Category = "Sports"},
+                new ProductCategory(){Category = "Fashion"},
+                new ProductCategory(){Category = "Books"},
+                new ProductCategory(){Category = "Video Games"},
+                new ProductCategory(){Category = "Health"},
+                new ProductCategory(){Category = "Movies & TV"}
+            };
+
+            productCategories.ForEach(t => context.ProductCategories.Add(t));
 
             context.SaveChanges();
         }
