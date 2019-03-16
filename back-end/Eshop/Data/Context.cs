6761a6979bc141b56361badb063d7456e50096d7
Povilas <p.krasavinas@gmail.com>
2019-03-16 13:30:15 +0200

    Added service and data layers

--- back-end/Eshop/Data/Context.cs
+++ back-end/Eshop/Data/Context.cs
@@ -14,5 +14,7 @@ namespace Eshop.Data
         }
 
         public DbSet<User> Users { get; set; }
+
+        public DbSet<ProductCategory> ProductCategories { get; set; }
     }
 }
---
+++ back-end/Eshop/Services/Interfaces/IProductCategoriesService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Eshop.DTOs;
+using Eshop.Models;
+
+namespace Eshop.Services.Interfaces
+{
+    public interface IProductCategoriesService
+    {
+        Task<ICollection<ProductCategoryDto>> GetAll();
+    }
+}
