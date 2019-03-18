f71686fc6c3e4a39b00ec9f3174620ebe73939bb
Povilas <p.krasavinas@gmail.com>
2019-03-18 16:09:55 +0200

    Code maid cleanup

--- back-end/Eshop/Data/Repositories/ProductsRepository.cs
+++ back-end/Eshop/Data/Repositories/ProductsRepository.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Eshop.Models;
+﻿using Eshop.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace Eshop.Data.Repositories
 {
--- back-end/Eshop/Temp/ProductCategory.cs
+++ back-end/Eshop/Temp/ProductCategory.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Eshop.Temp
+﻿namespace Eshop.Temp
 {
     public enum ProductCategory
     {
