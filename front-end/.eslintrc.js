8b9785b7388edcab7ee2b0a1a9de7137eb4e711a
Gvidas <gvidaxs@gmail.com>
2019-04-19 16:58:16 +0300

    Created eslint rules

--- front-end/.eslintrc.js
+++ front-end/.eslintrc.js
@@ -11,6 +11,8 @@ module.exports = {
     'no-underscore-dangle': 'off',
     'react/jsx-one-expression-per-line': 'off',
     'import/prefer-default-export': 'off',
+    'jsx-a11y/interactive-supports-focus': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
     radix: 'off'
   },
   globals: {
