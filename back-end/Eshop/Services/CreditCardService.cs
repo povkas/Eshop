7c34b2e38a764f7f043675dc22eff9505f0fa900
Povilas <p.krasavinas@gmail.com>
2019-04-21 20:46:49 +0300

    Added Credit Card service

---
+++ back-end/Eshop/Services/CreditCardService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Eshop.Data.Repositories;
+using Eshop.DTOs.CreditCards;
+using Eshop.Models;
+using Eshop.Services.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace Eshop.Services
+{
+    public class CreditCardService : ICreditCardService
+    {
+        private IRepository<CreditCard> _repository;
+        private readonly IMapper _mapper;
+
+        public CreditCardService(IRepository<CreditCard> repository,
+            IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<ICollection<CreditCardDto>> GetAll()
+        {
+            var creditCards = await _repository.GetAll();
+            var creditCardDto = _mapper.Map<CreditCardDto[]>(creditCards);
+
+            return creditCardDto;
+        }
+    }
+}
---
+++ back-end/Eshop/Services/Interfaces/ICreditCardService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Eshop.Models;
+
+namespace Eshop.Services.Interfaces
+{
+    public class ICreditCardService
+    {
+        Task<ICollection<CreditCard>> GetAll;
+    }
+}
