440b7c4a41bf512f9046b76c8a46d530e7f6639c
Povilas <p.krasavinas@gmail.com>
2019-04-21 20:35:15 +0300

    Added CreditCard data transfer object

---
+++ back-end/Eshop/DTOs/CreditCards/CreditCardDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eshop.DTOs.CreditCards
+{
+    public class CreditCardDto
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public string ExpirationDate { get; set; }
+        public int SecretNumber { get; set; }
+    }
+}
