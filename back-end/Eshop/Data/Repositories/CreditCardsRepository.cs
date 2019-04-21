b144d7e402fd1b04c49af4c9d99f9f1bc282a56a
Povilas <p.krasavinas@gmail.com>
2019-04-21 20:27:43 +0300

    Added Credit Card repository

---
+++ back-end/Eshop/Data/Repositories/CreditCardsRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Eshop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eshop.Data.Repositories
+{
+    public class CreditCardsRepository : RepositoryBase<CreditCard>
+    {
+        protected override DbSet<CreditCard> ItemSet { get; }
+
+        public CreditCardsRepository(Context context) : base(context)
+        {
+            ItemSet = context.CreditCards;
+        }
+
+        protected override IQueryable<CreditCard> IncludeDependencies(IQueryable<CreditCard> queryable)
+        {
+            return queryable;
+        }
+    }
+}
