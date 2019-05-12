f16d4ec6283f8c4a4da956170e3d8430a86eeb53
deividas <deivarnagis@gmail.com>
2019-05-12 17:52:27 +0300

    login validation

--- front-end/src/validation/login.js
+++ front-end/src/validation/login.js
@@ -1,23 +1,27 @@
 import { authErrors } from '../utils/constants';
 
 export const validateEmail = email => {
-  const { validEmail } = authErrors;
+  const { validEmail, isRequiredEmail } = authErrors;
   const emailRegex = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/;
 
   if (email.length !== 0) {
     if (email.length > 128 || !emailRegex.test(email)) {
       return validEmail;
     }
+  } else {
+    return isRequiredEmail;
   }
   return '';
 };
 
 export const validatePassword = password => {
-  const { passwordLength } = authErrors;
+  const { passwordLength, isRequiredPassword } = authErrors;
   if (password.length !== 0) {
     if (password.length < 8 || password.length > 255) {
       return passwordLength;
     }
+  } else {
+    return isRequiredPassword;
   }
   return '';
 };
