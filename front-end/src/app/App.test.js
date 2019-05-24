f48f87933fe4991970ee08e21ebd43cc9980af54
Gvidas <gvidaxs@gmail.com>
2019-05-24 18:04:11 +0300

    removed a test

--- front-end/src/app/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import App from './App';
-
-describe('Main body tests', () => {
-  it('Main body renders', () => {
-    expect(mount(<App />));
-  });
-});
